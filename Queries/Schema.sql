d08ef315c1a00077197791079789aab5533d4ad8
LaurenMcQuillan <mcquillan.888@gmail.com>
2022-11-28 16:15:28 -0500

    Added retirement queries

--- Queries/Schema.sql
+++ Queries/Schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE employees (
 
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -44,15 +44,18 @@ CREATE TABLE titles (
 	title varchar NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no,title,from_date)
 );
 
 CREATE TABLE dept_employee (
-	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+	emp_no INT NOT NULL,
+	dept_no VARCHAR() NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
+
+DROP TABLE dept_employee;
+
+DROP TABLE titles;
